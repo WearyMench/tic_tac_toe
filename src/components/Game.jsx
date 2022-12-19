57fd31938f0a7ab65035f6ce22e64b077207c742
Adrian07 <adrian.mirabal07@gmail.com>
2022-12-19 11:51:32 -0400

    css changes

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,11 +1,7 @@
 import React, { useState } from "react";
 import { calculateWinner } from "../helpers";
 import Board from "./Board";
-
-const style = {
-  width: "200px",
-  margin: "20px auto",
-};
+import "../style.css";
 
 const Game = () => {
   const [board, setBoard] = useState(Array(9).fill(null));
@@ -23,13 +19,15 @@ const Game = () => {
   };
 
   const renderMoves = () => (
-    <button onClick={() => setBoard(Array(9).fill(null))}>Start Game</button>
+    <button className="button-65" onClick={() => setBoard(Array(9).fill(null))}>
+      Start Game
+    </button>
   );
 
   return (
     <>
       <Board squares={board} onClick={handleClick} />
-      <div style={style}>
+      <div className="gamediv">
         <p>
           {winner ? `Winner: ${winner}` : `Next player: ${xIsNext ? "X" : "O"}`}
         </p>
