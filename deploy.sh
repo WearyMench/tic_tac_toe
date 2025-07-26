5048f5a95f4f1235097ccba943cc472888f7925f
Adrian Mirabal <adrian.mirabal07@gmail.com>
2025-07-26 12:56:26 -0400

    Update deployment scripts and configuration for consistent naming and homepage URL

--- deploy.sh
+++ deploy.sh
@@ -24,4 +24,4 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:WearyMench/tic-tac-toe.git main:gh-pages
+git push -f git@github.com:WearyMench/tic_tac_toe.git main:gh-pages
